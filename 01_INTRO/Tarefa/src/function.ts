7eed2e782d1294f4766db9e5a4cccdcd46f9ee6d
AlexCol <eu_axil@yahoo.com.br>
2024-05-31 12:46:01 -0300

    finaliza primeira sessao

---
+++ 01_INTRO/Tarefa/src/function.ts
@@ -0,0 +1,3 @@
+export const Soma = (n1: number, n2: number): number => {
+  return n1 + n2;
+};
---
+++ 01_INTRO/Tarefa/src/index.ts
@@ -0,0 +1,7 @@
+import { Soma } from "./function";
+
+const numero1 = 2;
+const numero2 = 3;
+const resultado = Soma(numero1, numero2);
+
+console.log(`A soma de ${numero1} e ${numero2} é ${resultado}.`);
