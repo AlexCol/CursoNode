906d4c631ff6a2acf74b97f9295f625978f02203
AlexCol <eu_axil@yahoo.com.br>
2024-11-26 13:12:45 -0300

    iniciando projeto front end, que pra variar vou fazer diferente do do professor (de javascript com react puro para typescript e next)

---
+++ 15_GET_A_PET/frontend/src/app/(main)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  console.log("renderizou");
+  return (
+    <div className="Home">
+      "Hello, world!"
+    </div>
+  );
+}
---
+++ 15_GET_A_PET/frontend/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  console.log("renderizou about");
+  return (
+    <div className="About">
+      "Hello, about!"
+    </div>
+  );
+}
