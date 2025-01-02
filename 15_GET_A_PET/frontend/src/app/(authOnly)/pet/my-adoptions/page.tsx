6ee391e94294671f46306dfc7fde5314670c888b
Alexandre B Coletti <alexandre.coletti@controlsoft.com.br>
2025-01-02 13:18:24 -0300

    adicionando tela de visualização e finalização de adoção

--- 15_GET_A_PET/frontend/src/app/(authOnly)/pet/my-adoptions/page.tsx
+++ 15_GET_A_PET/frontend/src/app/(authOnly)/pet/my-adoptions/page.tsx
@@ -1,8 +1,77 @@
-import React from 'react'
+'use client';
+
+import useFlashMessage from '@/hooks/useFlashMessage';
+import { Pet } from '@/interfaces/Pets';
+import api from '@/utils/api';
+import React, { useEffect, useState } from 'react'
+import styles from '../Pets.module.css';
+import RoundedImage from '@/components/layout/RoundedImage';
+import Link from 'next/link';
+import { User } from '@/interfaces/User';
 
 function MyAdoptions() {
+  const [pets, setPets] = useState<Pet[]>([]);
+  const [owners, setOwners] = useState<User[]>([]);
+
+  function getOwnerData(ownerId: string) {
+    const ownerData = owners.find((owner) => owner._id === ownerId);
+    return ownerData;
+  }
+
+  useEffect(() => {
+    async function FecthAdoptions() {
+      let searchedOwners: User[] = [];
+      try {
+        const response = await api.get(`/pets/myadoptions`); //lembrete: já tem um Bearer token no header (anexado no useAuth)
+        setPets(response.data);
+        console.log(response.data);
+
+        const uniqueValuesOfOwners = Array.from(new Set(pets.map((pet) => pet.user)));
+        uniqueValuesOfOwners.map(async (ownerId) => {
+          const response = await api.get(`/users/${ownerId}`);
+          owners.push(response.data);
+        });
+        setOwners(searchedOwners);
+      } catch (error) {
+        useFlashMessage().setFlashMessage(`Erro ao carregar Adoções: ${error}`, 'error');
+        setPets([]);
+      }
+    }
+    FecthAdoptions();
+  }, []);
+
   return (
-    <div>MyAdoptions</div>
+    <section>
+      <div className={styles.petlist_header}>
+        <h1>Minhas adoções</h1>
+      </div>
+      <div className={styles.petlist_container}>
+        {pets.length > 0 ? (
+          pets.map((pet) => (
+            <div key={pet._id} className={styles.petlist_row}>
+              <RoundedImage
+                src={pet.images ? `${process.env.NEXT_PUBLIC_API}/images/pets/${pet.images[0]}` : `${process.env.NEXT_PUBLIC_API}/images/no-image.jpg`}
+                alt={pet.name}
+                width='px75'
+              />
+              <span className='bold'>{pet.name}</span>
+              <div className={styles.actions}>
+                {pet.available ? (
+                  <>
+                    <p>Adoção em processo!</p>
+                  </>
+                ) : (
+                  <p>Parabens por concluir a adoção!</p>
+                )}
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Ainda não há adoções de Pets.</p>
+        )}
+      </div>
+
+    </section>
   )
 }
 
--- 15_GET_A_PET/frontend/src/app/(free)/pet/[id]/page.tsx
+++ 15_GET_A_PET/frontend/src/app/(free)/pet/[id]/page.tsx
@@ -8,12 +8,35 @@ import useFlashMessage from '@/hooks/useFlashMessage';
 import api from '@/utils/api';
 import { UserContext } from '@/contexts/user/userContext';
 import Link from 'next/link';
+import { AxiosError } from 'axios';
 
 function PetDetails() {
   const { id } = useParams();
   const { authenticated } = useContext(UserContext);
   const [pet, setPet] = useState<Pet>({} as Pet);
   const { setFlashMessage } = useFlashMessage();
+
+  async function schedule() {
+    let msgType = 'success';
+    let msgText = 'Visita agendada com sucesso!';
+    try {
+      const data = await api.patch(`/pets/schedule/${id}`); //lembrete: já tem um Bearer token no header (anexado no useAuth)
+
+    } catch (error) {
+      msgType = 'error';
+      msgText = '';
+      if (error instanceof AxiosError) {
+        if (error.response?.data?.Error) {
+          msgText = error.response?.data?.Error;
+        }
+      }
+
+      if (!msgText) {
+        msgText = `Erro ao agendar visita: ${error}`;
+      }
+    }
+    setFlashMessage(msgText, msgType);
+  }
 
   useEffect(() => {
     const fetchPet = async () => {
@@ -53,7 +76,7 @@ function PetDetails() {
           </p>
 
           {authenticated ? (
-            <button>Solicitar uma visita</button>
+            <button onClick={schedule}>Solicitar uma visita</button>
           ) : (
             <p>
               Você precisa <Link href='/auth/login'>logar</Link> para solicitar a visita.
